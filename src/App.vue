2ab9a63e1f463785c5d280d263500a922fd2e0cc
FlameWolf <agnijith@gmail.com>
2026-07-20 14:45:16 +0530

    Add event demo component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <script setup lang="ts">
 	import "@/styles.css";
+	import { ref } from "vue";
+	import { emptyString } from "@/constants/common";
 	import Navbar from "@/components/Navbar.vue";
+	import EventDemo from "@/components/EventDemo.vue";
 	import PropsDemo from "@/components/PropsDemo.vue";
 	import RefDemo from "@/components/RefDemo.vue";
 	import WatchDemo from "@/components/WatchDemo.vue";
 	import FormDemo from "@/components/FormDemo.vue";
 	import ConfirmModal from "@/components/ConfirmModal.vue";
+
+	const age = ref(25);
+	const ageChangeMessage = ref(emptyString);
+	function onAgeIncrease(step: number) {
+		age.value += step;
+		ageChangeMessage.value = `Age increased: new age: ${age.value}`;
+	}
+	function onAgeDecrease(step: number) {
+		age.value -= step;
+		ageChangeMessage.value = `Age decreased: new age: ${age.value}`;
+	}
 </script>
 <template>
 	<Navbar/>
+	<EventDemo name="Iris" :age greeting="Hello" :age-step="2" @increase-age="onAgeIncrease" @decrease-age="onAgeDecrease"/>
+	<p>{{ ageChangeMessage }}</p>
 	<PropsDemo name="Jane" :age="20" greeting="Hello"/>
 	<RefDemo/>
 	<WatchDemo/>
---
+++ src/components/EventDemo.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+	const { name, age, greeting, ageStep } = defineProps<{
+		name: string;
+		age: number;
+		greeting: string;
+		ageStep?: number;
+	}>();
+	const emit = defineEmits({
+		increaseAge: (years: number) => {
+			if (typeof years !== "number") {
+				return false;
+			}
+			return true;
+		},
+		decreaseAge: (years: number) => {
+			if (typeof years !== "number") {
+				return false;
+			}
+			return true;
+		}
+	});
+	const currentYear = new Date().getFullYear();
+	const yearsToAdd = 5;
+</script>
+<template>
+	<h2>Props Demo</h2>
+	<hr/>
+	<p>{{ greeting }}, {{ name }}.</p>
+	<p>You are {{ age }} years old.</p>
+	<p>In {{ currentYear + yearsToAdd }} you will be {{ age + yearsToAdd }} years old.</p>
+	<div class="d-flex gap-2">
+		<button class="btn btn-primary" @click="emit(`increaseAge`, ageStep ?? 1)">Increase Age</button>
+		<button class="btn btn-primary" @click="emit('decreaseAge', ageStep ?? 1)">Decrease Age</button>
+	</div>
+	<hr/>
+</template>
