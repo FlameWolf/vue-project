5aa8053b9c602644fb9c9271bf8786d394a9916a
FlameWolf <agnijith@gmail.com>
2026-07-20 16:56:53 +0530

    Add slot demo component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 	import { ref } from "vue";
 	import { emptyString } from "@/constants/common";
 	import Navbar from "@/components/Navbar.vue";
+	import SlotDemo from "@/components/SlotDemo.vue";
 	import EventDemo from "@/components/EventDemo.vue";
 	import PropsDemo from "@/components/PropsDemo.vue";
 	import RefDemo from "@/components/RefDemo.vue";
@@ -12,6 +13,8 @@
 
 	const age = ref(25);
 	const ageChangeMessage = ref(emptyString);
+	const currentDate = ref(new Date());
+
 	function onAgeIncrease(step: number) {
 		age.value += step;
 		ageChangeMessage.value = `Age increased: new age: ${age.value}`;
@@ -23,6 +26,15 @@
 </script>
 <template>
 	<Navbar/>
+	<SlotDemo>
+		<p>This content is displayed inside the slot.</p>
+		<hr/>
+		<p>The date now is {{ currentDate.toISOString() }}</p>
+		<hr/>
+		<button class="btn btn-primary" @click="currentDate = new Date()">Click</button>
+		<hr/>
+		<p>End of slot content.</p>
+	</SlotDemo>
 	<EventDemo name="Iris" :age greeting="Hello" :age-step="2" @increase-age="onAgeIncrease" @decrease-age="onAgeDecrease"/>
 	<p>{{ ageChangeMessage }}</p>
 	<PropsDemo name="Jane" :age="20" greeting="Hello"/>
