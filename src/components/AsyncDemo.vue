be2ac85825b0270b01c93e4671e9637e0d4e12cd
FlameWolf <agnijith@gmail.com>
2026-08-06 17:50:59 +0530

    Add spinner and error components; update async component loader function with options

--- src/components/AsyncDemo.vue
+++ src/components/AsyncDemo.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
 	import { defineAsyncComponent, useTemplateRef } from "vue";
+	import Spinner from "@/components/Spinner.vue";
+	import Error from "@/components/Error.vue";
 
-	const testComp = defineAsyncComponent(() => import("./TestComponent.vue"));
+	const testComp = defineAsyncComponent({
+		loader: async () => {
+			await new Promise(resolve => setTimeout(resolve, 2000));
+			return import("./TestComponent.vue");
+		},
+		loadingComponent: Spinner,
+		errorComponent: Error
+	});
 	const asyncRef = useTemplateRef("async-ref");
 </script>
 <template>
 	<div>
 		<h2>Async Component Demo</h2>
-		<test-comp ref="async-ref"/>
+		<test-comp ref="async-ref" />
 		<p><code>stringRef</code> exposed by the async component is: {{ asyncRef?.stringRef }}</p>
 		<p><code>numRef</code> exposed by the async component is: {{ asyncRef?.numRef }}</p>
 		<p><code>funcRef</code> exposed by the async component is: {{ asyncRef?.funcRef }}</p>
 	</div>
-	<hr/>
+	<hr />
 </template>
---
+++ src/components/Error.vue
@@ -0,0 +1,6 @@
+<template>
+	<div class="alert alert-danger" role="alert">
+		<h4 class="alert-heading">Error</h4>
+		<p>There was an error loading the component.</p>
+	</div>
+</template>
