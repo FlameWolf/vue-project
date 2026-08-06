ea79fc97d8b22487f09fcabb1a67469c0b6d33fd
FlameWolf <agnijith@gmail.com>
2026-08-06 17:18:29 +0530

    Add symbol key and factory function example for provide/inject

--- src/components/InjectDemo.vue
+++ src/components/InjectDemo.vue
@@ -2,11 +2,20 @@
 	import { inject } from "vue";
 
 	const count = inject("count", 0);
+	const time = inject(
+		Symbol.for("time"),
+		() => {
+			const date = new Date();
+			return `Locally computed: ${date.toLocaleTimeString()}`;
+		},
+		true
+	);
 </script>
 <template>
 	<div>
 		<h2>Inject Demo</h2>
 		<p>Count: {{ count }}</p>
+		<p>Time: {{ time }}</p>
 	</div>
-	<hr/>
+	<hr />
 </template>
--- src/components/ProvideDemo.vue
+++ src/components/ProvideDemo.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-	import { provide, ref } from "vue";
+	import { computed, provide, ref } from "vue";
 	import InjectContainer from "@/components/InjectContainer.vue";
 
 	const count = ref(0);
+	const time = ref(new Date().toLocaleTimeString());
 	provide("count", count);
+	provide(Symbol.for("time"), computed(() => `Supplied from provider: ${time.value}`));
 </script>
 <template>
 	<div>
