9863bcc4f2d7a9243c60d587b0a7dbb8901f848b
FlameWolf <agnijith@gmail.com>
2026-08-07 18:13:01 +0530

    Update `useFetch` composable and its demo component to use dynamic URL parameter

--- src/components/ComposableDemo.vue
+++ src/components/ComposableDemo.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
 	import { ref, watch } from "vue";
+	import { emptyString } from "@/constants/common";
 	import { useFetch } from "@/composables/useFetch";
 	import Spinner from "@/components/Spinner.vue";
 
 	const isLoading = ref(true);
-	const { data, error } = useFetch("https://jsonplaceholder.typicode.com/posts/1");
+	const url = ref("https://jsonplaceholder.typicode.com/posts/1");
+	const { data, error } = useFetch(url);
+
+	function reFetch() {
+		url.value = emptyString;
+		isLoading.value = true;
+		requestAnimationFrame(() => {
+			url.value = "https://jsonplaceholder.typicode.com/posts/2";
+		});
+	}
 
 	watch([data, error], () => {
 		isLoading.value = false;
@@ -13,11 +23,14 @@
 <template>
 	<div>
 		<h2>Composable Demo</h2>
-		<Spinner v-if="isLoading"/>
+		<Spinner v-if="isLoading" />
 		<div v-else-if="error">Error: {{ error.message }}</div>
 		<div v-else>
 			<h2>{{ data?.title }}</h2>
 			<p>{{ data?.body }}</p>
 		</div>
+		<hr />
+		<button class="btn btn-primary" @click="reFetch">Fetch Post 2</button>
 	</div>
+	<hr />
 </template>
--- src/composables/useFetch.ts
+++ src/composables/useFetch.ts
@@ -1,22 +1,30 @@
-import { ref, toValue, type MaybeRefOrGetter } from "vue";
+import { ref, toRef, toValue, watch, watchEffect, type MaybeRefOrGetter } from "vue";
 
 export function useFetch(url: MaybeRefOrGetter<string>, init?: RequestInit) {
 	const data = ref<Record<keyof any, any> | null>(null);
 	const error = ref<Error | null>(null);
 
-	fetch(toValue(url), init)
-		.then(response => {
-			if (!response.ok) {
-				error.value = new Error(`Error fetching data. Status: ${response.status}`);
-			}
-			return response.json();
-		})
-		.then(json => {
-			data.value = json;
-		})
-		.catch(err => {
-			error.value = err;
-		});
+	function fetchData() {
+		const resolvedUrl = toValue(url);
+		if(!resolvedUrl) {
+			return;
+		}
+		fetch(resolvedUrl, init)
+			.then(response => {
+				if (!response.ok) {
+					error.value = new Error(`Error fetching data. Status: ${response.status}`);
+				}
+				return response.json();
+			})
+			.then(json => {
+				data.value = json;
+			})
+			.catch(err => {
+				error.value = err;
+			});
+	}
+
+	watch(toRef(url), fetchData, { immediate: true });
 
 	return {
 		data,
